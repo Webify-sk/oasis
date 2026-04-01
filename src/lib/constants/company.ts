9de06d0a5696b440bcda9d72bff0a768ab9f2862
Webify <bkonecny45@gmail.com>
2026-04-01 16:35:45 +0200

    ICO/DIC invoice fix

--- src/lib/constants/company.ts
+++ src/lib/constants/company.ts
@@ -1,10 +1,10 @@
 
 export const COMPANY_DETAILS = {
     name: 'Oasis Lounge s.r.o.',
-    address: 'Vajnorská 100/A\n831 04 Bratislava',
-    ico: '12 345 678', // Placeholder
-    dic: '2021234567', // Placeholder
-    icdph: 'SK2021234567', // Placeholder
+    address: 'Pribinova 25\n811 09 Bratislava - mestská časť Staré Mesto\nSlovensko',
+    ico: 'IČO: 56418078',
+    dic: 'DIČ: 2122300895',
+    icdph: 'IČ DPH: SK2122300895',
     email: 'info@oasis-lounge.sk',
     phone: '+421 900 000 000',
     web: 'www.oasislounge.sk'
